d81edf408c4f30dc168a1e44e805e8773b838cbc
ULADZIMIR <63464909+vovantais@users.noreply.github.com>
2022-04-06 23:05:42 +0300

    feat add staticrecurse

---
+++ visualForseTrenning/staticresources/script.js
@@ -0,0 +1,9 @@
+function openModal() {
+        const popup = document.querySelector('.popup');
+        popup.style = 'display: block';
+}
+
+function closeModal() {
+        const popup = document.querySelector('.popup');
+        popup.style = 'display: none';
+}
