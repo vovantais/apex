6844569395f8db2819355c80ef9290316fc2c1a7
ULADZIMIR <63464909+vovantais@users.noreply.github.com>
2022-08-25 09:59:49 +0300

    feat add logic

--- visualForseTrenning/staticresources/script.js
+++ visualForseTrenning/staticresources/script.js
@@ -7,3 +7,22 @@ function closeModal() {
         const popup = document.querySelector('.popup');
         popup.style = 'display: none';
 }
+
+function closeSuccessMessage() {
+  const popup = document.querySelector('.content__formOrder-SuccessSend');
+  popup.style = 'display: none';
+}
+
+function displayCurrentPrice() {
+  const slider = document.querySelector(".slider");
+  const output = document.querySelector(".price");
+  output.innerHTML = slider.value + '$';
+
+  slider.oninput = function() {
+    output.innerHTML = this.value + '$';
+  }
+}
+
+(function init() {
+  displayCurrentPrice();
+})();
